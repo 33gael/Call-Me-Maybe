9c0f712c9b815284202cb20da93cc88561d3215f
Gael Ducasse <33gael.dev@gmail.com>
2026-08-11 16:50:30 +0200

    f push

--- explore.py
+++ explore.py
@@ -1,10 +1,10 @@
-from llm_sdk.llm_sdk import Small_LLM_Model
+from llm_sdk import Small_LLM_Model
 
-def print_loading() -> None:
+def init_model() -> None:
     """init model"""
     print("Model loading")
     llm = Small_LLM_Model() #init model
     print("Model ready")
 
 if __name__ == "__main__":
-    print_loading()
+    init_model()
--- src/main.py
+++
@@ -1,3 +0,0 @@
-def ft() -> None:
-    """Just a function for testing my main"""
-    print("Hello from call-me-maybe!")
