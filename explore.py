c67cc1c3c178d2cf440a065cf38646adfcc7daff
Gaël <33gael.dev@gmail.com>
2026-08-14 18:58:19 +0200

    good return type for llm init

--- explore.py
+++ explore.py
@@ -1,12 +1,14 @@
 from llm_sdk import Small_LLM_Model
 
+llm = Small_LLM_Model() #init model
+
 def ft_encode(str):
     """try using encode()"""
     print("Model loading")
-    llm = Small_LLM_Model() #init model
     print("Model ready")
     res = llm.encode(str)
-    return (print(res))
+    print(res)
+    return (res)
 
 if __name__ == "__main__":
     str = "What is the sum of 2 and 3?"
