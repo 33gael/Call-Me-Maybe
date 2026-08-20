db489e5d7beb21d50fdc03f5adf566ced20d5fdd
Gaël <33gael.dev@gmail.com>
2026-08-20 14:51:25 +0200

    print type of voc_file + len of voc available in the file

--- explore.py
+++ explore.py
@@ -6,12 +6,12 @@
 #    By: gaeducas <gaeducas@student.fr>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/08/20 12:01:30 by gaeducas          #+#    #+#              #
-#    Updated: 2026/08/20 12:01:32 by gaeducas         ###   ########.fr        #
+#    Updated: 2026/08/20 14:50:03 by gaeducas         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from llm_sdk import Small_LLM_Model
-
+import json
 
 #  init model
 print("Model loading")
@@ -34,6 +34,16 @@ def ft_decode(prompt):
     return (integer)
 
 
+def vocab_file() -> str:
+    file = llm.get_path_to_vocab_file()
+    with open(file) as f:
+        loaded_file = json.load(f)
+        print(type(loaded_file))
+        print(len(loaded_file))
+    return (file)
+
+
 if __name__ == "__main__":
     prompt = "What is the sum of 2 and 3?"
     ft_decode(prompt)
+    vocab_file()
