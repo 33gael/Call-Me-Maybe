d3199cefdafd25531157b3d0e276df49b9f9a523
Gaël <33gael.dev@gmail.com>
2026-08-21 23:47:38 +0200

    update AGENTS.md

--- explore.py
+++ explore.py
@@ -6,7 +6,7 @@
 #    By: gaeducas <gaeducas@student.fr>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/08/20 12:01:30 by gaeducas          #+#    #+#              #
-#    Updated: 2026/08/21 19:34:01 by gaeducas         ###   ########.fr        #
+#    Updated: 2026/08/21 23:39:44 by gaeducas         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -41,9 +41,11 @@ def vocab_file() -> str:
         file_items = loaded_file.items()
         print(type(loaded_file))
         print(len(loaded_file))
-        for tuple in file_items:
-            print(tuple)
-            break
+        for tok_pair in file_items:
+            print(tok_pair)
+            if tok_pair == 220:
+                print(repr(tok_pair))
+                break
     return (file)
 
 
